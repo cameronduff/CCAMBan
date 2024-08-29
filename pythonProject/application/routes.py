132b907999bcd34e2a4b48bca9f593e79571ec4d
Cameron Duff <cameronsduff@gmail.com>
2024-08-29 12:06:45 +0100

    Updating file structure

--- pythonProject/application/routes.py
+++ pythonProject/application/routes.py
@@ -6,3 +6,15 @@ from application import app
 @app.route('/home')
 def home():
     return render_template('home.html', title='Home')
+
+@app.route('/meetTheTechGrads')
+def meetTheTechGrads():
+    return render_template('MeetTheTechGrads.html')
+
+@app.route('/meetTheRoles')
+def meetTheRoles():
+    return render_template('MeetTheRoles.html')
+
+@app.route('/skyGlossary')
+def skyGlossary():
+    return render_template('SkyGlossary.html')
